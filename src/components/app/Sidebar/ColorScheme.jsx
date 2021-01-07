cb0e2947b9669d89a9b731628d1a970adae393d5
michmitz <stermitzm@gmail.com>
2021-01-07 13:13:28 -0800

    Implemented color scheme for now

--- src/components/app/Sidebar/ColorScheme.jsx
+++ src/components/app/Sidebar/ColorScheme.jsx
@@ -1,9 +1,71 @@
 import React from 'react';
+import styles from '../../../styles/styles.css';
+import sidebarStyles from '../../../styles/sidebarStyles.css';
+const randomColor = require('randomcolor');
+
+const handleChange = (e) => {
+  const selectedColor = e.target.value;
+
+  const generatedColor = randomColor({ luminosity: 'dark' });
+  const generatedColor2 = randomColor({ luminosity: 'light' });
+  
+  if(selectedColor === 'light') {
+    document.documentElement.style
+      .setProperty('--text', 'black');
+    document.documentElement.style
+      .setProperty('--background', 'white');
+    document.documentElement.style
+      .setProperty('--line', 'black'); 
+  } else if(selectedColor === 'dark') {
+    document.documentElement.style
+      .setProperty('--text', 'white');
+    document.documentElement.style
+      .setProperty('--background', 'black');
+    document.documentElement.style
+      .setProperty('--line', 'white');
+  } else if(selectedColor === 'random') {
+    document.documentElement.style
+      .setProperty('--text', generatedColor);
+    document.documentElement.style
+      .setProperty('--background', generatedColor2);
+    document.documentElement.style
+      .setProperty('--line', generatedColor);
+  }
+};
+
 
 export default function ColorScheme() {
   return (
-    <div>
-      <h1>Color Scheme</h1>
+    <div className={sidebarStyles.colorSchemeDiv}>
+      <h2>Color Scheme</h2>
+
+      <label>
+        <input 
+          type="radio" 
+          name="color" 
+          value="light"
+          onChange={handleChange}  />
+            Light
+      </label>
+
+      <label>
+        <input 
+          type="radio" 
+          name="color" 
+          value="dark"
+          onChange={handleChange}  />
+            Dark
+      </label>
+
+      <label>
+        <input 
+          type="radio" 
+          name="color" 
+          value="random"
+          onChange={handleChange} />
+            Random
+      </label>
+
     </div>
   );
 }
