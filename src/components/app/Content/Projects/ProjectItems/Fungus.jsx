e33a68dbbef51fdf1d263aabdccffb809694f88c
michmitz <stermitzm@gmail.com>
2021-01-14 23:07:09 -0800

    Made ProjectItem for FungusAmongUs, implemented AwesomeSlider

---
+++ src/components/app/Content/Projects/ProjectItems/Fungus.jsx
@@ -0,0 +1,32 @@
+/* eslint-disable max-len */
+import React from 'react';
+import styles from '../../../../../styles/styles.css';
+import AwesomeSlider from 'react-awesome-slider';
+import builtInSliderStyles from 'react-awesome-slider/dist/styles.css';
+import projectStyles from '../../../../../styles/projectStyles.css';
+
+export default function Projects() {
+
+  const fungusSlider = (
+    <AwesomeSlider cssModule={builtInSliderStyles}>
+      <div>
+        <img src={'/assets/project-images/fungus-1.png'} className={projectStyles.sliderImage}/>
+      </div>
+      <div>
+        <img src={'/assets/project-images/fungus-2.png'} className={projectStyles.sliderImage}/>
+      </div>
+      <div>
+        <img src={'/assets/project-images/fungus-3.png'} className={projectStyles.sliderImage}/>
+      </div>
+    </AwesomeSlider>
+  );
+
+  return (
+    <div className={projectStyles.slider}>{fungusSlider}
+      <h2 className={projectStyles.projectName}>FungusAmongUs</h2>
+      <h3 className={projectStyles.tech}>Tech: JavaScript, HTML, CSS</h3>
+      <p>FungusAmongUs is an interactive, educational app that allows users to select a geographical region of Oregon and learn more about the mushrooms commonly found in that region. The user can then opt to "collect" that mushroom and add it to their mushroom basket for reference later.</p>
+      <p>FungusAmongUs was built in a four day sprint with JavaScript, HTML, and CSS. I heavily contributed to the app's functionality by navigating the setup of our JavaScript functions to render the mushrooms on each page.</p>
+    </div>
+  );
+}
--- src/components/app/Content/Projects/Projects.jsx
+++ src/components/app/Content/Projects/Projects.jsx
@@ -3,13 +3,16 @@ import React from 'react';
 import FadeIn from 'react-fade-in';
 import { Link } from 'react-router-dom';
 import styles from '../../../../styles/styles.css';
+import Fungus from './ProjectItems/Fungus';
+import projectStyles from '../../../../styles/projectStyles.css';
 
 export default function Projects() {
   return (
     <FadeIn transitionDuration={2000} className={styles.contentDiv}>
-      <div className={styles.contentDiv}>
-        <Link to="/" className={styles.returnLink}>Return</Link>
-        <header className={styles.contentHeader}>Past Projects</header>
+      <Link to="/" className={styles.returnLink}>Return</Link>
+      <header className={styles.contentHeader}>Past Projects</header>
+      <div className={projectStyles.sliderDiv}>
+        <Fungus />
       </div>
     </FadeIn>
   );
