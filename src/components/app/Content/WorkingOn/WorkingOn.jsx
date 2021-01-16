e62c67d87887e526c53964d04386161ef0605f64
michmitz <stermitzm@gmail.com>
2021-01-15 23:10:46 -0800

    Added Forecast project

--- src/components/app/Content/WorkingOn/WorkingOn.jsx
+++ src/components/app/Content/WorkingOn/WorkingOn.jsx
@@ -3,14 +3,22 @@ import React from 'react';
 import FadeIn from 'react-fade-in';
 import { Link } from 'react-router-dom';
 import styles from '../../../../styles/styles.css';
+import projectStyles from '../../../../styles/projectStyles.css';
+import Forecast from './WorkingOnItems/Forecast';
 
 export default function WorkingOn() {
   return (
     <FadeIn transitionDuration={2000} className={styles.contentDiv}>
-      <div className={styles.contentDiv}>
-        <Link to="/" className={styles.returnLink}>Return</Link>
-        <header className={styles.contentHeader}>Current Projects</header>
+      
+      <Link to="/" className={styles.returnLink}>Return</Link>
+        
+      <header className={styles.contentHeader}>Current Projects</header>
+        
+      <div className={projectStyles.sliderDiv}>
+        <Forecast />
       </div>
+    
     </FadeIn>
   );
 }
+
