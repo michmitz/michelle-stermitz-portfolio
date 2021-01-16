8fef754e1963d1f34227a63157bbebe7a77a1709
michmitz <stermitzm@gmail.com>
2021-01-15 19:30:17 -0800

    updated FungusAmongUs project item

--- src/components/app/Content/Projects/ProjectItems/Fungus.jsx
+++ src/components/app/Content/Projects/ProjectItems/Fungus.jsx
@@ -22,11 +22,28 @@ export default function Fungus() {
   );
 
   return (
-    <div className={projectStyles.slider}>{fungusSlider}
-      <h2 className={projectStyles.projectName}>FungusAmongUs</h2>
+    <div className={projectStyles.slider}>
+      <a href="https://the-fun-guys.github.io/FungusAmongUs/" className={projectStyles.projectName}>FungusAmongUs</a>
+      
+      {fungusSlider}
+
       <h3 className={projectStyles.tech}>Tech: JavaScript, HTML, CSS</h3>
+
       <p className={projectStyles.description}>FungusAmongUs is an interactive, educational app that allows users to select a geographical region of Oregon and learn more about the mushrooms commonly found in that region. The user can then opt to "collect" that mushroom and add it to their mushroom basket for reference later.</p>
+      
       <p className={projectStyles.description}>FungusAmongUs was built in a four day sprint with JavaScript, HTML, and CSS. I heavily contributed to the app's functionality by navigating the setup of our JavaScript functions to render the mushrooms on each page.</p>
+      
+      <div className={projectStyles.lineDiv} />
+
+      <label className={projectStyles.githubLabel}>
+        <img className={projectStyles.githubIcon} src={'/assets/GitHub-Mark.png'} />
+        <a href="https://github.com/The-Fun-Guys/FungusAmongUs" className={projectStyles.githubLink}>Source Code</a>
+      </label>
+
+      <label className={projectStyles.websiteLabel}>
+        <a href="https://the-fun-guys.github.io/FungusAmongUs/" className={projectStyles.websiteLink}>Website</a>
+      </label>
+    
     </div>
   );
 }
