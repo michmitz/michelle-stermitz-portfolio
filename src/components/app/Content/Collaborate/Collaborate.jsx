1c7571a84e41ad142b4a3aaaa08529d97247695f
michmitz <stermitzm@gmail.com>
2021-01-19 18:45:00 -0800

    Added contact form

--- src/components/app/Content/Collaborate/Collaborate.jsx
+++ src/components/app/Content/Collaborate/Collaborate.jsx
@@ -2,12 +2,56 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import styles from '../../../../styles/styles.css';
+import collaborateStyles from '../../../../styles/collaborateStyles.css';
+import FadeIn from 'react-fade-in';
 
 export default function Collaborate() {
   return (
-    <div className={styles.contentDiv}>
+    <FadeIn transitionDuration={2000} className={styles.contentDiv}>
       <Link to="/" className={styles.returnLink}>Return</Link>
       <header className={styles.contentHeader}>Collaborate</header>
-    </div>
+
+      <div className={collaborateStyles.formDiv}>
+        <form
+          className={collaborateStyles.form}
+          name="contact-form"
+          method="POST"
+        >
+          <input type="hidden" name="form-name" value="websiteForm" action="/success" />
+
+          <label className={collaborateStyles.formLabel}>
+            <input
+              className={collaborateStyles.inputs}
+              type="text"
+              name="name"
+              placeholder="name"
+              required
+            />
+          </label>
+
+          <label className={collaborateStyles.formLabel}>
+            <input
+              className={collaborateStyles.inputs}
+              type="email"
+              name="email"
+              placeholder="email"
+              required
+            />
+          </label>
+
+          <label className={collaborateStyles.formLabel}>
+            <textarea
+              className={collaborateStyles.message}
+              name="message"
+              placeholder="message"
+              required
+            ></textarea>
+          </label>
+          <p>
+            <button type="submit" className={collaborateStyles.sendButton}>SEND</button>
+          </p>
+        </form>
+      </div>
+    </FadeIn>
   );
 }
